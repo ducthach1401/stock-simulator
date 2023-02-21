3de9d9b01e99512b4528d7a99bd831f131a48106
Đức Thạch <ducthach.1401@gmail.com>
2023-02-21 10:47:03 +0700

    add deploy

--- build.sh
+++ build.sh
@@ -1,11 +1,6 @@
 #!/bin/bash
 
-cp * ./deploy
-
-cd deploy
 npm run build
-
-rm -rf src docker test node_modules
 
 docker build --pull --rm -f "Dockerfile" -t ducthach1401/stock-simulator:0.0.1 "."
 docker push ducthach1401/stock-simulator:0.0.1
