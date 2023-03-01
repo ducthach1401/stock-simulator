dcbe30e00998feaf95528360624fd98f9a1e1ec9
thach <ducthach.1401@gmail.com>
2023-03-01 16:41:15 +0700

    update build

--- build.sh
+++ build.sh
@@ -8,4 +8,5 @@ npm run build
 
 docker login --username=$DOCKER_USER --password=$DOCKER_PASS
 docker build --pull --rm -f "Dockerfile" -t ducthach1401/${name}:${version} "."
+docker build --pull --rm -f "Dockerfile" -t ducthach1401/${name}:lastest "."
 docker push ducthach1401/${name}:${version}
