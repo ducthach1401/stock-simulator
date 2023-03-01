d2c4ab40dd40859c899971c36608cf1e9bb48d77
thach <ducthach.1401@gmail.com>
2023-03-01 16:56:11 +0700

    test ci cd

--- src/modules/app/app-service.ts
+++ src/modules/app/app-service.ts
@@ -6,6 +6,7 @@ export class AppService {
   constructor(private readonly configService: ConfigService) {}
 
   getHello(): string {
+    console.log('test');
     return `${this.configService.get<string>('app.name')} / ${new Date()}`;
   }
 }
